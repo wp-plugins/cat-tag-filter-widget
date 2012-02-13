db2bdc70c64caa2ad982e804399334400d4ad5d0
ajayver <ajayver@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-13 12:42:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cat-tag-filter-widget/trunk@504267 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cat-tag-filter.php
+++ cat-tag-filter.php
@@ -1,30 +1,19 @@
 <?php
 /*
 Plugin Name: Cat + Tag Filter
-Plugin URI: http://wordpress.org/extend/plugins/cat-tag-filter-widget/
+Plugin URI: 
 Description: This plugin adds a widget to your WordPress site that allows your visitors to filter posts by category and tag.
 Author: Ajay Verma
-Version: 0.3
+Version: 0.3.1
 Author URI: http://traveliving.org/
-License: GPL2
 */
 /*  Copyright 2011  Verma Ajay  (email : ajayverma1986@gmail.com)
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    
 */
-
 
 if ($_POST["ctf_submit"] == 1) {
 	$tag_prefix = '?tag=';
-	if ($_POST["tag_logic"] == "AND") $tag_logic = '+';
+	if ($_POST["tag_logic"] == 1) $tag_logic = '+';
 	else $tag_logic = ',';
 	if ($_POST["cat"] != -1) {
 		$cat = '/?category_name=' . $_POST['cat'];
@@ -86,7 +75,7 @@ function cat_options(){
 function tag_options($type){
   global $ctf_options, $current_tag; 
   $tags = get_tags();
-  if ($type == 'checkboxes'){
+  if ($type == 1){
    $options .= '<ul>';
   foreach ($tags as $tag) {
    $options .= '<li>'; 
@@ -103,7 +92,7 @@ function tag_options($type){
   }
   else {  
  
- $options .= '<select name="tag" id="ctf-tag-select" ><option value="-1">';
+ $options .= '<select name="' . "tag[]" . '" id="ctf-tag-select" ><option value="-1">';
   if ($ctf_options['all_tags_text'] != '') $options .= $ctf_options['all_tags_text']; else $options .=__('Any tag', 'cat-tag-filter');
   $options .= '</option>'; 
   foreach ($tags as $tag) {
@@ -152,7 +141,7 @@ class cat_tag_filter extends WP_Widget {
   }
   /** @see WP_Widget::widget */
   function widget($args, $instance) {	
-	  $defaults = array( 'title' => __('Filter', 'cat-tag-filter'), 'button_title' => __('Show posts', 'cat-tag-filter'), 'cat_list_label' => __('Show posts from:', 'cat-tag-filter'), 'tag_list_label' => __('With tag:', 'cat-tag-filter'), 'all_cats_text' => __('Any category', 'cat-tag-filter'), 'all_tags_text' => __('Any tag', 'cat-tag-filter'), 'cats_count' => 1, 'tags_count' => 0, 'tag_logic' => 'AND', 'tag_type' => 'dropdown' );
+	  $defaults = array( 'title' => __('Filter', 'cat-tag-filter'), 'button_title' => __('Show posts', 'cat-tag-filter'), 'cat_list_label' => __('Show posts from:', 'cat-tag-filter'), 'tag_list_label' => __('With tag:', 'cat-tag-filter'), 'all_cats_text' => __('Any category', 'cat-tag-filter'), 'all_tags_text' => __('Any tag', 'cat-tag-filter'), 'cats_count' => 1, 'tags_count' => 0, 'tag_logic' => 1, 'tag_type' => 0 );
     $instance = wp_parse_args( (array) $instance, $defaults );
     extract( $args );
     global $ctf_options;
@@ -188,9 +177,14 @@ class cat_tag_filter extends WP_Widget {
   }
   /** @see WP_Widget::form */
   function form($instance) {   
-    $defaults = array( 'title' => __('Filter', 'cat-tag-filter'), 'button_title' => __('Show posts', 'cat-tag-filter'), 'cat_list_label' => __('Show posts from:', 'cat-tag-filter'), 'tag_list_label' => __('With tag:', 'cat-tag-filter'), 'all_cats_text' => __('Any category', 'cat-tag-filter'), 'all_tags_text' => __('Any tag', 'cat-tag-filter'), 'cats_count' => 1, 'tags_count' => 0, 'tag_logic' => 'AND', 'tag_type' => 'dropdown' );
+    $defaults = array( 'title' => __('Filter', 'cat-tag-filter'), 'button_title' => __('Show posts', 'cat-tag-filter'), 'cat_list_label' => __('Show posts from:', 'cat-tag-filter'), 'tag_list_label' => __('With tag:', 'cat-tag-filter'), 'all_cats_text' => __('Any category', 'cat-tag-filter'), 'all_tags_text' => __('Any tag', 'cat-tag-filter'), 'cats_count' => 1, 'tags_count' => 0, 'tag_logic' => 1, 'tag_type' => 0 );
   	$instance = wp_parse_args( (array) $instance, $defaults ); 				
-    ?>          
+    ?>   
+	<p>
+		<strong>Please consider donating 10 cents if you like this plugin! :)</strong>	</p>
+		<p>
+	<a href="http://goo.gl/SCdKg"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" alt="donate" /></a>    
+	</p>
     <p>    
       <label for="ctf-widget-title">
         <?php _e('Widget title', 'cat-tag-filter'); ?>: 
@@ -227,18 +221,32 @@ class cat_tag_filter extends WP_Widget {
       </label>    
       <input type="text" id="ctf-all-tags-text" name="<?php echo $this->get_field_name('all_tags_text'); ?>" value="<?php echo esc_attr($instance['all_tags_text']);?>" />   
     </p>  
-	<p>        
-      <input type="checkbox" id="ctf-tag-type" name="<?php echo $this->get_field_name('tag_type'); ?>" value="checkboxes" <?php if ($instance['tag_type'] == 'checkboxes') echo "checked ";?>/>
-      <label for="ctf-tag-type">
-        <?php _e('Allow selecting multiple tags with checkboxes', 'cat-tag-filter'); ?> 
-      </label>   
-    </p>
-	<p>        
-      <input type="checkbox" id="ctf-tag-logic" name="<?php echo $this->get_field_name('tag_logic'); ?>" value="AND" <?php if ($instance['tag_logic'] == 'AND') echo "checked ";?>/>
-      <label for="ctf-tag-logic">
-        <?php _e('Use AND logic for multiple tags instead of OR', 'cat-tag-filter'); ?> 
-      </label>   
-    </p>
+	<p>
+	<label for="ctf-tag-type">
+	<?php _e('How to show tags:', 'cat-tag-filter'); ?> 
+	</label>
+	<select name="<?php echo $this->get_field_name('tag_type'); ?>" id="ctf-tag-type" >   
+      <option value="0" <?php if ($instance['tag_type'] == 0) echo 'selected="selected" ';?> >
+      <?php _e('Dropdown list', 'cat-tag-filter'); ?>
+      </option>   
+	  <option value="1" <?php if ($instance['tag_type'] == 1) echo 'selected="selected" ';?> >
+      <?php _e('Checkboxes', 'cat-tag-filter'); ?>
+      </option>
+    </select>
+	</p>
+	<p>
+	<label for="ctf-tag-logic">
+	<?php _e('Logic operator for multiple tags:', 'cat-tag-filter'); ?> 
+	</label>
+	<select name="<?php echo $this->get_field_name('tag_logic'); ?>" id="ctf-tag-logic" >   
+      <option value="0" <?php if ($instance['tag_logic'] == 0) echo 'selected="selected" ';?> >
+      <?php _e('OR', 'cat-tag-filter'); ?>
+      </option>   
+	  <option value="1" <?php if ($instance['tag_logic'] == 1) echo 'selected="selected" ';?> >
+      <?php _e('AND', 'cat-tag-filter'); ?>
+      </option>
+    </select>
+	</p>
     <p>        
       <input type="checkbox" id="ctf-tags-count" name="<?php echo $this->get_field_name('tags_count'); ?>" value="1" <?php if ($instance['tags_count'] == 1) echo "checked ";?>/>
       <label for="ctf-tags-count">
@@ -251,16 +259,7 @@ class cat_tag_filter extends WP_Widget {
       </label>    
       <input type="text" id="ctf-button-title" name="<?php echo $this->get_field_name('button_title'); ?>" value="<?php echo esc_attr($instance['button_title']);?>" />   
     </p>  
-	<p>
-	Please consider donating 10 cents if you like this plugin! :)
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHTwYJKoZIhvcNAQcEoIIHQDCCBzwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYB86djK8SOM+oNjSMaJOe3ilNm9dS1hZ9mp61hjJ+u9MAF0ZylvJ8b/Xa+/ONEYGG4fu5AAiXfTmwBZolx8UUYUg2cqUjo+sWE3BEJBhzGXaip5HeFTQm0Jt8HpCWsZ1+xLgNdasNXHkCJMj29VvH9d1CcA6Oj0DLxQjLNgcIqolDELMAkGBSsOAwIaBQAwgcwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIoWPRXJQIdSKAgaiIe7dBbaDtu6CVkxuMRjLjJAlaxUoEELqXEkE2ezNpUX1XbZ475xvv9PIzbw122burAdQ8HF+pF/ejvA9FRfZBcyIUZMdatn8qEsSSiwnnh2Bb7mM16F5jLBs+B0XA9dle5n/zhwkrY1LpwxSFxvAwkOD7yqu6jYvvakLePgGADM7crr7mBeHDpP9Ka4hBX4ob9hHJPQq9nz+oY5+8O9PTPl0+OgUa0oSgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xMjAyMTMwOTIyMTRaMCMGCSqGSIb3DQEJBDEWBBR22eggjw84w5wmRZx3fNRX5/aClTANBgkqhkiG9w0BAQEFAASBgGjkQIJJE+SMJGOtZAWvX2ApHmXrwLVgbNgTV1On5errRH9Px3Pm8ulolJbcO8JaDHGaVCqDdhlM1+r+BoeVQTiOvwliVULQ12y9qqNil+2+cSfjo1MHsSJaebY6MxRX8EcYMkUiNMuXrFWMcqacpwmSyk/cfK+4jSYyB/9r5AoS-----END PKCS7-----
-">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
-	</p>
+	
   <?php 
   }
 } // class Cat + Tag Filter
